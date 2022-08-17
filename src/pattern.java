6d6210b5a2644e4b23dd5e7c926bfba660521ab9
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-08-17 15:07:41 +0530

    updated

--- src/pattern.java
+++ src/pattern.java
@@ -1,36 +1,51 @@
 public class pattern {
     public static void main(String[] args) {
         int n = 5;
-        for(int i=0; i<n; ++i){
-            for(int j=0; j<i+1; ++j){
-                System.out.print("*"+" ");
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i + 1; ++j) {
+                System.out.print("*" + " ");
             }
             System.out.println();
         }
         System.out.println();
-        for(int i=0; i<n; ++i){
-            for(int j=0; j<n; ++j){
-                System.out.print("*"+" ");
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                System.out.print("*" + " ");
             }
             System.out.println();
         }
         System.out.println();
-        for(int i=0; i<n; ++i){
-            for(int j=0; j<n-i; ++j){
-                System.out.print("*"+" ");
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n - i; ++j) {
+                System.out.print("*" + " ");
             }
             System.out.println();
         }
         System.out.println();
 
-        for(int i = 0; i<n; ++i){
-            for (int j = 0;j<n+i; ++j){
-                if (j<i*2)
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n + i; ++j) {
+                if (j < i * 2)
                     System.out.print("  ");
                 else
-                    System.out.print("*"+" ");
+                    System.out.print("* ");
             }
             System.out.println();
         }
+        System.out.println();
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (j < n - i - 1)
+                    System.out.print("  ");
+                else
+                    System.out.print("* ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
     }
 }
